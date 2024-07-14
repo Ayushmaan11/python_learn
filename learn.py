946c0ef649270908ebeef491bb4d89873a6a451a
Ayushmaan11 <118163907+Ayushmaan11@users.noreply.github.com>
2024-07-14 01:48:34 -0700

    Add files via upload

---
+++ learn.py
@@ -0,0 +1,35 @@
+from math import *
+
+print("Hello World")
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+character_name = "dev"
+character_age = 35
+is_male=False
+
+print(f"Ayushmaan is {character_age} years old")
+print(character_name+" is 30 years \n old")
+
+phrase = "Breaking Bad"
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase.index("e"))
+print(phrase.replace("Bad","Jesse"))
+
+my_num = 5
+print(my_num)
+num = -2
+print(abs(num))
+print(pow(4,2))
+print(max(5,2))
+print(min(4,2))
+print(round(3.2))
+print(round(4.7))
---
+++ learn2.py
@@ -0,0 +1,14 @@
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+total  = float(num1)+float(num2)
+
+print(f"The sum will be {total}")
+
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter name of celebrity")
+
+print("roses are " +color)
+print(plural_noun+" are blue")
+print("I Love " +celebrity)
